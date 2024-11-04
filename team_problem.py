3c4c98aff8d0229329a458b837e8e4e528bc27a6
Md Sazzad Sarker <sazzadsarker49@gmail.com>
2024-11-04 19:18:57 +0600

    Update team_problem.py

--- team_problem.py
+++ team_problem.py
@@ -1,16 +1,7 @@
-# Input the number of problems
 n = int(input())
-
-# Initialize a counter for the number of problems they will implement
 implement_count = 0
-
-# Loop through each problem's input
 for _ in range(n):
-    # Read the input for each friend's certainty
     petya, vasya, tonya = map(int, input().split())
-    # Check if at least two friends are sure about the solution
     if petya + vasya + tonya >= 2:
         implement_count += 1
-
-# Output the count of problems they will implement
 print(implement_count)
